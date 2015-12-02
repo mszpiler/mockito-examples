78b0307c468dc2c4e9036515b21f3fcef6dfe4ad
mszpiler <mariusz.szpiler@gmail.com>
2015-12-02 21:42:52 +0100

    Update MyFinalClass.java

--- src/main/java/com/enotion/MyFinalClass.java
+++ src/main/java/com/enotion/MyFinalClass.java
@@ -1,8 +1,8 @@
 package com.enotion;
 
-/**
- * Created by jan on 26.11.2015.
- */
+/*
+This is example which shows that when you have final class then you can't stub it using Mockito
+*/
 public final class MyFinalClass {
     public String method() {
         System.out.println("Can't change my behaviour!");
